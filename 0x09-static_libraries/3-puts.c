e08bae4bcb5090935f44316446cd798b82bad58b
imelda-1 <im.ishimwe@gmail.com>
2023-03-10 14:10:05 -0800

    task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - print string to stdout
+ * @s: the string to print
+ */
+void _puts(char *s)
+{
+	puts (s);
+}
